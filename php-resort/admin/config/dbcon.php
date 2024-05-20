c79cb4b6f419c72bb0f8a8b74f61551463554add
lawrence122302 <lawrencelasmarias1@gmail.com>
2024-05-20 15:38:17 +0800

    Changed database

--- php-resort/admin/config/dbcon.php
+++ php-resort/admin/config/dbcon.php
@@ -3,7 +3,7 @@
 $host = "localhost";
 $username = "root";
 $password = "";
-$database = "angela";
+$database = "dbAngela";
 
 try{
     $con = mysqli_connect($host, $username, $password, $database);
