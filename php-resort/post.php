d301a856d7ccad0a3a1ab99e580e1824f591d1e3
lawrence122302 <lawrencelasmarias1@gmail.com>
2024-07-04 05:01:46 +0800

    post view by slug and design post view page

--- php-resort/post.php
+++ php-resort/post.php
@@ -7,9 +7,56 @@ include('includes/navbar.php');
     <div class="container">
         <div class="row">
             
-
             <div class="col-md-9">
-                <h2>This is your Post View</h2>
+                
+            <?php
+                if(isset($_GET['title']))
+                {
+                    $slug = mysqli_real_escape_string($con, $_GET['title']);
+
+                    $posts = "SELECT * FROM posts WHERE slug='$slug' ";
+                    $posts_run = mysqli_query($con, $posts);
+
+                    if(mysqli_num_rows($posts_run) > 0)
+                    {
+                        foreach($posts_run as $postItems)
+                        {
+                            ?>
+                                <div class="card shadow-sm mb-4">
+                                    <div class="card-header">
+                                        <h5><?=$postItems['name'];?></h5>
+                                    </div>
+                                   
+                                    <div class="card-body">
+                                        <label class="text-dark me-2">Posted On: <?= date('d-M-Y', strtotime($postItems['created_at'])); ?></label>
+                                        <hr/>
+                                        <?php if($postItems['image'] != null) : ?>
+                                        <img src="uploads/posts/<?= $postItems['image'] ?>" class="w-50" alt="<?=$postItems['name'];?>" />
+                                        <?php endif; ?>
+                                        <div>
+                                            <?=$postItems['description'];?>
+                                        </div>
+                                    </div>
+                                </div>
+                            <?php
+                        }
+                        
+                    }
+                    else
+                    {
+                        ?>
+                            <h4>No Such Category Found</h4>
+                        <?php
+                    }
+                }
+                else
+                {
+                    ?>
+                        <h4>No Such Post Found</h4>
+                    <?php
+                }
+                ?>
+
             </div>
 
             <div class="col-md-3">
