ec383d0abc927a4d7a2ed52517821adee87143d6
lawrence122302 <lawrencelasmarias1@gmail.com>
2024-09-04 14:51:12 +0800

    admin features and facilities

--- rbwebsite/admin/inc/header.php
+++ rbwebsite/admin/inc/header.php
@@ -22,7 +22,7 @@
                         <a class="nav-link text-white" href="user_queries.php">User Queries</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link text-white" href="users.php">Users</a>
+                        <a class="nav-link text-white" href="carousel.php">Carousel</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link text-white" href="settings.php">Settings</a>
