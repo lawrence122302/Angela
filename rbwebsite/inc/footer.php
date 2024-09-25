c441d936545e0846ce7ff4ad500c8d63eff7ea7a
lawrence122302 <lawrencelasmarias1@gmail.com>
2024-09-25 18:50:18 +0800

    confirm booking page setup & booking availability validations
    1:00:14

--- rbwebsite/inc/footer.php
+++ rbwebsite/inc/footer.php
@@ -189,7 +189,15 @@
             }
             else
             {
-                window.location = window.location.pathname;
+                let fileurl = window.location.href.split('/').pop().split('?').shift();
+                if(fileurl == 'room_details.php')
+                {
+                    window.location = window.location.href;
+                }
+                else
+                {
+                    window.location = window.location.pathname;
+                }
             }
         }
         xhr.send(data);
@@ -242,6 +250,18 @@
         }
         xhr.send(data);
     });
+
+    function checkLoginToBook(status,room_id)
+    {
+        if(status)
+        {
+            window.location.href='confirm_booking.php?id='+room_id;
+        }
+        else
+        {
+            alert('error','Please login to book room!');
+        }
+    }
 
     setActive();
 </script>
