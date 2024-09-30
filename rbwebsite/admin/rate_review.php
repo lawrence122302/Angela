b652c0037f5f1662e35798804af819f0792e56e7
lawrence122302 <lawrencelasmarias1@gmail.com>
2024-09-30 23:39:28 +0800

    review and rating

--- rbwebsite/admin/rate_review.php
+++ rbwebsite/admin/rate_review.php
@@ -9,7 +9,7 @@
 
         if($frm_data['seen']=='all')
         {
-            $q = "UPDATE user_queries SET seen=?";
+            $q = "UPDATE rating_review SET seen=?";
             $values = [1];
             if(update($q,$values,'i'))
             {
@@ -22,7 +22,7 @@
         }
         else
         {
-            $q = "UPDATE user_queries SET seen=? WHERE sr_no=?";
+            $q = "UPDATE rating_review SET seen=? WHERE sr_no=?";
             $values = [1,$frm_data['seen']];
             if(update($q,$values,'ii'))
             {
@@ -41,7 +41,7 @@
 
         if($frm_data['del']=='all')
         {
-            $q = "DELETE FROM user_queries";
+            $q = "DELETE FROM rating_review";
             if(mysqli_query($con,$q))
             {
                 alert('success','All data deleted!');
@@ -53,7 +53,7 @@
         }
         else
         {
-            $q = "DELETE FROM user_queries WHERE sr_no=?";
+            $q = "DELETE FROM rating_review WHERE sr_no=?";
             $values = [$frm_data['del']];
             if(delete($q,$values,'i'))
             {
@@ -72,7 +72,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Panel - User Queries</title>
+    <title>Admin Panel - Ratings & Reviews</title>
     <?php require('inc/links.php'); ?>
 </head>
 <body class="bg-light">
@@ -82,7 +82,7 @@
     <div class="container-fluid" id="main-content">
         <div class="row">
             <div class="col-lg-10 ms-auto p-4 overflow-hidden">
-                <h3 class="mb-4">User Queries</h3>
+                <h3 class="mb-4">Ratings & Reviews</h3>
 
                 <div class="card border-0 shadow-sm mb-4">
                     <div class="card-body">
@@ -96,42 +96,47 @@
                         </a>
                     </div>
 
-                    <div class="table-responsive-md" style="height: 450px; overflow-y: scroll;">
+                    <div class="table-responsive-md">
                         <table class="table table-hover border">
                             <thead>
                                 <tr class="bg-dark text-light">
                                     <th scope="col">#</th>
-                                    <th scope="col">Name</th>
-                                    <th scope="col">Email</th>
-                                    <th scope="col" width="20%">Subject</th>
-                                    <th scope="col" width="30%">Message</th>
+                                    <th scope="col">Room Name</th>
+                                    <th scope="col">User Name</th>
+                                    <th scope="col">Rating</th>
+                                    <th scope="col" width="30%">Review</th>
                                     <th scope="col">Date</th>
                                     <th scope="col">Action</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <?php
-                                    $q = "SELECT * FROM user_queries ORDER BY sr_no DESC";
+                                    $q = "SELECT rr.*,uc.name AS uname, r.name AS rname FROM rating_review rr 
+                                        INNER JOIN user_cred uc ON rr.user_id = uc.id
+                                        INNER JOIN rooms r ON rr.room_id = r.id
+                                        ORDER BY sr_no DESC";
+
                                     $data = mysqli_query($con,$q);
                                     $i=1;
 
                                     while($row = mysqli_fetch_assoc($data))
                                     {
+                                        $date = date('d-m-Y', strtotime($row['datentime']));
                                         $seen='';
                                         if($row['seen']!=1)
                                         {
-                                            $seen = "<a href='?seen=$row[sr_no]' class='btn btn-sm rounded-pill btn-primary'>Mark as read</a> <br>";
+                                            $seen = "<a href='?seen=$row[sr_no]' class='btn btn-sm rounded-pill btn-primary mb-2'>Mark as read</a> <br>";
                                         }
-                                        $seen.="<a href='?del=$row[sr_no]' class='btn btn-sm rounded-pill btn-danger mt-2'>Delete</a>";
+                                        $seen.="<a href='?del=$row[sr_no]' class='btn btn-sm rounded-pill btn-danger'>Delete</a>";
 
                                         echo<<<query
                                             <tr>
                                                 <td>$i</td>
-                                                <td>$row[name]</td>
-                                                <td>$row[email]</td>
-                                                <td>$row[subject]</td>
-                                                <td>$row[message]</td>
-                                                <td>$row[date]</td>
+                                                <td>$row[rname]</td>
+                                                <td>$row[uname]</td>
+                                                <td>$row[rating]</td>
+                                                <td>$row[review]</td>
+                                                <td>$date</td>
                                                 <td>$seen</td>
                                             </tr>
                                         query;
