f3974d74b3ae965f2decb71e21c0581eed077b00
lawrence122302 <lawrencelasmarias1@gmail.com>
2024-11-07 11:36:04 +0800

    phone number enabled symbols fix

--- rbwebsite/admin/ajax/settings_crud.php
+++ rbwebsite/admin/ajax/settings_crud.php
@@ -54,9 +54,32 @@
     {
         $frm_data = filteration($_POST);
 
-        $q = "UPDATE contact_details SET address=?,gmap=?,pn1=?,pn2=?,email=?,fb=?,insta=?,tw=?,iframe=? WHERE sr_no=?";
-        $values = [$frm_data['address'],$frm_data['gmap'],$frm_data['pn1'],$frm_data['pn2'],$frm_data['email'],$frm_data['fb'],$frm_data['insta'],$frm_data['tw'],$frm_data['iframe'],1];
-        $res = update($q,$values,'sssssssssi');
+        $q = "UPDATE contact_details 
+            SET address = ?, 
+                gmap = ?, 
+                pn1 = ?, 
+                pn2 = ?, 
+                email = ?, 
+                fb = ?, 
+                insta = ?, 
+                tw = ?, 
+                iframe = ? 
+            WHERE sr_no = 1";
+
+        $values = [
+            $frm_data['address'],
+            $frm_data['gmap'],
+            $frm_data['pn1'],
+            $frm_data['pn2'],
+            $frm_data['email'],
+            $frm_data['fb'],
+            $frm_data['insta'],
+            $frm_data['tw'],
+            $frm_data['iframe']
+        ];
+
+        $res = update($q, $values, 'sssssssss');
+
         echo $res;
     }
 ?>
