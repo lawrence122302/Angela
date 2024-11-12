83f43d1f7c07491c9c195cab7c36de7fafa75763
lawrence122302 <lawrencelasmarias1@gmail.com>
2024-11-12 19:16:36 +0800

    faqs added

--- rbwebsite/frequently_asked_questions.php
+++ rbwebsite/frequently_asked_questions.php
@@ -4,14 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
     <?php require('inc/links.php'); ?>
-    <title><?php echo $settings_r['site_title'] ?> - About</title>
+    <title><?php echo $settings_r['site_title'] ?> - Virtual Tour</title>
     <style>
-        .box {
-            border-top-color: var(--teal) !important;
-        }
-
         body {
             font-family: 'Arial', sans-serif;
             background-color: #f4f4f4;
@@ -89,64 +84,6 @@
 
     <?php require('inc/navbar.php'); ?>
 
-    <div class="my-5 px-4">
-        <h2 class="fw-bold h-font text-center">About Us</h2>
-        <div class="h-line bg-dark"></div>
-        <p class="text-center mt-3">
-            Angela’s Private Pool offers a serene escape in Antipolo, 
-            featuring private pools with stunning city views,<br> 
-            perfect for family gatherings and special celebrations<br> 
-            just minutes from the Cathedral.
-        </p>
-    </div>
-
-    <div class="container">
-        <div class="row justify-content-between align-items-center">
-            <div class="col-lg-6 col-md-5 mb-4 order-lg-1 order-md-1 order-2">
-                <h3 class="mb-3">Get to know us!</h3>
-                <p>
-                    Angela’s Private Pool offers a tranquil retreat in Antipolo, Rizal, with seven private pools designed for comfort and relaxation, each offering stunning city views.<br>
-                    Located minutes from the Antipolo Cathedral, our resort is perfect for those seeking a peaceful getaway from the city’s hustle.<br><br>
-                    Ideal for family celebrations, gatherings, or a day of leisure, our resort provides spacious accommodations, air-conditioned rooms, grilling areas, and kitchen facilities to ensure a comfortable stay.<br>
-                    Our dedicated team strives to make each visit memorable, from booking to check-out.<br><br>
-                    At Angela’s Private Pool, we’re more than just a resort—we’re a place to relax, connect, and celebrate life’s special moments.
-                </p>
-            </div>
-            <div class="col-lg-5 col-md-5 mb-4 order-lg-2 order-md-2 order-1">
-                <img src="images/settings/footer.jpeg" class="w-100">
-            </div>
-        </div>
-    </div>
-
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-3 col-md-6 mb-4 px-4">
-                <div class="bg-white rounded shadow p-4 border-top border-4 text-center box h-100 d-flex flex-column justify-content-center align-items-center">
-                    <img src="images/about/swimming-pool.png" width="70px">
-                    <h4 class="mt-3">7 Angelas</h4>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6 mb-4 px-4">
-                <div class="bg-white rounded shadow p-4 border-top border-4 text-center box h-100 d-flex flex-column justify-content-center align-items-center">
-                    <img src="images/about/customers.svg" width="70px">
-                    <h4 class="mt-3">200+ Customers</h4>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6 mb-4 px-4">
-                <div class="bg-white rounded shadow p-4 border-top border-4 text-center box h-100 d-flex flex-column justify-content-center align-items-center">
-                    <img src="images/about/rating.svg" width="70px">
-                    <h4 class="mt-3">100+ Reviews</h4>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6 mb-4 px-4">
-                <div class="bg-white rounded shadow p-4 border-top border-4 text-center box h-100 d-flex flex-column justify-content-center align-items-center">
-                    <img src="images/about/caretaker.png" width="70px">
-                    <h4 class="mt-3">7 Caretakers</h4>
-                </div>
-            </div>
-        </div>
-    </div>
-
     <div class="mt-5 mb-2 px-4">
         <h2 class="fw-bold h-font text-center">Frequently Asked Questions (FAQs)</h2>
         <div class="h-line bg-dark"></div>
@@ -217,8 +154,6 @@
 
     <?php require('inc/footer.php'); ?>
 
-    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-
     <script>
         document.querySelectorAll('.faq-header').forEach(item => {
             item.addEventListener('click', event => {
@@ -231,27 +166,6 @@
                 }
             })
         })
-        
-        var swiper = new Swiper(".mySwiper", {
-            spaceBetween: 40,
-            pagination: {
-                el: ".swiper-pagination",
-            },
-            breakpoints: {
-                320: {
-                    slidesPerView: 1,
-                },
-                640: {
-                    slidesPerView: 1,
-                },
-                768: {
-                    slidesPerView: 3,
-                },
-                1024: {
-                    slidesPerView: 3,
-                },
-            }
-        });
     </script>
     
 </body>
